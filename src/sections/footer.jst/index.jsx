bd069a4dbaa27f1b16f5246598bed23c6bc215f9
Tammarnoon <eszee10@gmail.com>
2024-11-05 13:31:00 +0700

    update contafcts

--- src/sections/footer.jst/index.jsx
+++ src/sections/footer.jst/index.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
     <div className="mb-20">
       <div className="flex justify-end text-primaryC">
         <span>
-          Contacts - eszee10@gmail.com - 0619031001
+          Contacts - tammarnoon.2001@gmail.com - 0619031001
 
         </span>
       </div>
